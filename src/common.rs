107008620944f769d13f6930d1bc8f5120e79a90
Jacob Reger <regerjacob@gmail.com>
2026-02-28 23:01:33 -0800

    negatives working now

--- src/common.rs
+++ src/common.rs
@@ -1,7 +1,7 @@
 use quote::format_ident;
-use syn::{ItemFn, Type, parse_quote};
+use syn::{Ident, ItemFn, Type, parse_quote};
 
-#[derive(darling::FromField)]
+#[derive(darling::FromField, Default)]
 #[darling(attributes(field))]
 pub struct Field {
     pub ident: Option<syn::Ident>,
@@ -30,22 +30,19 @@ impl Field {
 
     fn signed_get_impl(&self, size: usize) -> ItemFn {
         assert!(self.signed);
-        let ident = self.ident.clone().expect("Field ident expected");
-        let fn_ident = format_ident!("get_{ident}");
+        let fn_ident = self.get_fn_ident();
         let lsb = self.lsb;
         let msb = self.msb;
-        let field_size = msb - lsb + 1;
-        let field_mask = 2u32.pow(field_size as u32 - 1) - 1;
-        let filler = (2u32.pow(size as u32 - field_size as u32) - 1) << (field_size - 1);
+        let abs_field_mask = self.signed_field_mask_nosignbit();
         let ty = self.io_ty(size);
 
         parse_quote! {
             pub fn #fn_ident(&self) -> #ty {
                 let signed = (self.reg >> #msb) & 1;
                 if signed == 1 {
-                    ((signed << (#size - 1)) | #filler | self.reg & ((#field_mask << #lsb) >> #lsb)) as #ty
+                    (!(#abs_field_mask >> #lsb) | self.reg & (#abs_field_mask >> #lsb)) as #ty
                 } else {
-                    (self.reg & ((#field_mask << #lsb) >> #lsb)) as #ty
+                    ((self.reg & #abs_field_mask) >> #lsb) as #ty
                 }
             }
         }
@@ -53,17 +50,13 @@ impl Field {
 
     fn unsigned_get_impl(&self, size: usize) -> ItemFn {
         assert!(!self.signed);
-        let ident = self.ident.clone().expect("Field ident expected");
-        let fn_ident = format_ident!("get_{ident}");
+        let fn_ident = self.get_fn_ident();
         let ty = self.io_ty(size);
         let lsb = self.lsb;
-        let msb = self.msb;
-        let field_size = msb - lsb + 1;
-        let field_mask = 2u32.pow(field_size as u32) - 1;
+        let field_mask = self.field_mask();
         parse_quote! {
             pub fn #fn_ident(&self) -> #ty {
-                let end_trimmed = self.reg >> #lsb;
-                end_trimmed & #field_mask
+                (self.reg & #field_mask) >> #lsb
             }
         }
     }
@@ -76,23 +69,26 @@ impl Field {
         }
     }
 
+    fn signed_field_mask_nosignbit(&self) -> u32 {
+        assert!(self.signed);
+        self.field_mask() & !(1 << self.msb)
+    }
+
     fn unsigned_set_impl(&self, size: usize) -> ItemFn {
         assert!(!self.signed);
         let ty = self.io_ty(size);
-        let ident = self.ident.clone().expect("Field ident expected");
-        let fn_ident = format_ident!("set_{ident}");
-        let field_size = self.msb - self.lsb + 1;
-        let field_max = 2u32.pow(field_size as u32) - 1;
-        let field_mask = 2u32.pow(field_size as u32) - 1;
+        let fn_ident = self.set_fn_ident();
+        let field_max = self.field_max();
+        let field_mask = self.field_mask();
         let lsb = self.lsb;
         parse_quote! {
             pub fn #fn_ident(&mut self, val: #ty) -> std::result::Result<(), String> {
                 if val > #field_max {
                     return Err("".to_string());
                 }
-                self.reg = self.reg & !(#field_mask << #lsb);
-                let val = val << #lsb;
-                self.reg = self.reg | val;
+
+                self.reg = self.reg & !(#field_mask);
+                self.reg = self.reg | (val << #lsb);
 
                 Ok(())
             }
@@ -102,28 +98,64 @@ impl Field {
     fn signed_set_impl(&self, size: usize) -> ItemFn {
         assert!(self.signed);
         let ty = self.io_ty(size);
-        let ident = self.ident.clone().expect("Field ident expected");
-        let fn_ident = format_ident!("set_{ident}");
-        let field_size = self.msb - self.lsb + 1;
-        let field_mask = 2u32.pow(field_size as u32) - 1;
-        let lsb = self.lsb;
+        let fn_ident = self.set_fn_ident();
+        let field_mask = self.field_mask();
+        let abs_field_mask = self.signed_field_mask_nosignbit();
         let msb = self.msb;
+        let lsb = self.lsb;
+        let field_max = self.field_max();
+        let field_min = self.field_min();
 
         parse_quote! {
             pub fn #fn_ident(&mut self, mut val: #ty) -> std::result::Result<(), String> {
+                if val > #field_max as i32 || val < #field_min {
+                    return Err("".to_string());
+                }
+
                 let signed_bit = if val < 0 {
                     1 << #msb
                 } else {
                     0
                 };
-                val = val.abs();
 
-                self.reg = self.reg & !(#field_mask << #lsb);
-                self.reg = self.reg | signed_bit | val as u32;
+                self.reg = self.reg & !(#field_mask);
+                self.reg = self.reg | signed_bit | ((val as u32 & (#abs_field_mask >> #lsb)) << #lsb);
 
                 Ok(())
             }
         }
+    }
+
+    fn get_fn_ident(&self) -> Ident {
+        format_ident!("get_{}", self.ident.as_ref().unwrap())
+    }
+
+    fn set_fn_ident(&self) -> Ident {
+        format_ident!("set_{}", self.ident.as_ref().unwrap())
+    }
+
+    fn field_max(&self) -> u32 {
+        if self.signed {
+            2u32.pow(self.field_size() as u32 - 1) - 1
+        } else {
+            2u32.pow(self.field_size() as u32) - 1
+        }
+    }
+
+    fn field_min(&self) -> i32 {
+        if self.signed {
+            !self.field_max() as i32
+        } else {
+            0
+        }
+    }
+
+    fn field_size(&self) -> usize {
+        self.msb - self.lsb + 1
+    }
+
+    fn field_mask(&self) -> u32 {
+        (2u32.pow(self.field_size() as u32) - 1) << self.lsb
     }
 
     fn io_ty(&self, size: usize) -> Type {
@@ -158,5 +190,105 @@ impl Field {
             }
             _ => panic!("Invalid register size"),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_field_max() {
+        let field = Field {
+            msb: 3,
+            lsb: 0,
+            signed: false,
+            ..Default::default()
+        };
+        assert_eq!(field.field_max(), 15);
+
+        let field = Field {
+            msb: 16,
+            lsb: 10,
+            signed: true,
+            ..Default::default()
+        };
+        assert_eq!(field.field_max(), 63);
+    }
+
+    #[test]
+    fn test_field_min() {
+        let field = Field {
+            msb: 3,
+            lsb: 0,
+            signed: false,
+            ..Default::default()
+        };
+        assert_eq!(field.field_min(), 0);
+
+        let field = Field {
+            msb: 16,
+            lsb: 10,
+            signed: true,
+            ..Default::default()
+        };
+        assert_eq!(field.field_min(), -64);
+    }
+
+    #[test]
+    fn test_field_size() {
+        let field = Field {
+            msb: 3,
+            lsb: 0,
+            signed: false,
+            ..Default::default()
+        };
+        assert_eq!(field.field_size(), 4);
+
+        let field = Field {
+            msb: 16,
+            lsb: 10,
+            signed: true,
+            ..Default::default()
+        };
+        assert_eq!(field.field_size(), 7);
+    }
+
+    #[test]
+    fn test_field_mask() {
+        let field = Field {
+            msb: 3,
+            lsb: 0,
+            signed: false,
+            ..Default::default()
+        };
+        assert_eq!(field.field_mask(), 0b1111);
+
+        let field = Field {
+            msb: 16,
+            lsb: 10,
+            signed: true,
+            ..Default::default()
+        };
+        assert_eq!(field.field_mask(), 0b1_1111_1100_0000_0000);
+    }
+
+    #[test]
+    fn test_signed_abs_field_mask() {
+        let field = Field {
+            msb: 3,
+            lsb: 0,
+            signed: true,
+            ..Default::default()
+        };
+        assert_eq!(field.signed_field_mask_nosignbit(), 0b0111);
+
+        let field = Field {
+            msb: 16,
+            lsb: 10,
+            signed: true,
+            ..Default::default()
+        };
+        assert_eq!(field.signed_field_mask_nosignbit(), 0b0_1111_1100_0000_0000);
     }
 }
